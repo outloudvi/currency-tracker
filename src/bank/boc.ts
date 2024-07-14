57ec9508d1e6513861d786dcf648143ed662e82a
Outvi V <git@outv.im>
2024-07-14 22:50:20 +0800

    feat(boc): flow w/ thingproxy

--- src/bank/boc.ts
+++ src/bank/boc.ts
@@ -2,35 +2,41 @@ import dayjs from '../tools/day.ts'
 import { CurrencyGetterList, CurrencyResult } from '../types.ts'
 import { JSDOM } from 'jsdom'
 
-const getJpyCurrency = async (): Promise<CurrencyResult<'JPY'>> => {
+const getCurrency = async <T extends string>(
+  currencyMark: T,
+  currencyName: string
+): Promise<CurrencyResult<T>> => {
   const resultHtml = await fetch(
-    'https://srh.bankofchina.com/search/whpj/search_cn.jsp',
+    'https://thingproxy.freeboard.io/fetch/https://www.boc.cn/sourcedb/whpj/',
     {
       headers: {
         'User-Agent':
           'Mozilla/5.0 (Windows NT 10.0; rv:130.0) Gecko/20100101 Firefox/130.0',
       },
       referrer: 'https://www.boc.cn/',
-      body: `erectDate=&nothing=&pjname=${encodeURIComponent('日元')}`,
-      method: 'POST',
+      method: 'GET',
     }
   ).then((x) => x.text())
 
-  console.log(resultHtml)
   const {
     window: { document },
   } = new JSDOM(resultHtml)
-  const line = document.querySelector('div.BOC_main table tr:nth-child(2)')
+  const line = [
+    ...(document.querySelector('div.BOC_main > div > div > table > tbody')
+      ?.children ?? []),
+  ].find((x) => x.innerHTML.includes(currencyName))
   if (!line) {
-    throw new Error('No line found for BOC')
+    throw new Error(`No ${currencyName} found for BOC`)
   }
 
   return {
-    currency: 'JPY',
-    rate: Number((line.children[4] as HTMLTableCellElement).innerText),
+    currency: currencyMark,
+    rate: Number((line.children[4] as HTMLTableCellElement).innerHTML),
     date: dayjs(
-      (line.children[6] as HTMLTableCellElement).innerText,
-      'YYYY.MM.DD HH:mm:ss'
+      `${(line.children[6] as HTMLTableCellElement).innerHTML} ${
+        (line.children[7] as HTMLTableCellElement).innerHTML
+      }`,
+      'YYYY-MM-DD HH:mm:ss'
     )
       .tz('Asia/Shanghai')
       .toDate(),
@@ -38,7 +44,7 @@ const getJpyCurrency = async (): Promise<CurrencyResult<'JPY'>> => {
 }
 
 const methods: CurrencyGetterList<'JPY'> = {
-  JPY: getJpyCurrency,
+  JPY: () => getCurrency('JPY', '日元'),
 }
 
 export default methods

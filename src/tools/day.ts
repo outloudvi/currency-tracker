176e00bc2d61e5d03227990fc75ecdc81c98db6f
Outvi V <git@outv.im>
2025-10-04 21:52:55 +0800

    chore: use `npm:`

--- src/tools/day.ts
+++ src/tools/day.ts
@@ -1,7 +1,7 @@
 import dayjs from 'dayjs/'
-import dayCustomParseFormat from 'dayjs/plugin/customParseFormat'
-import dayUtc from 'dayjs/plugin/utc'
-import dayTz from 'dayjs/plugin/timezone'
+import dayCustomParseFormat from 'dayjs/plugin/customParseFormat.js'
+import dayUtc from 'dayjs/plugin/utc.js'
+import dayTz from 'dayjs/plugin/timezone.js'
 
 dayjs.extend(dayCustomParseFormat)
 dayjs.extend(dayUtc)
